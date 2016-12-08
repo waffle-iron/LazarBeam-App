e47cb124877557685ba4605a1f0855c77cbf229c
kjellverschuren <kjellverschuren@Kjells-MacBook-Pro.local>
2016-12-08 16:15:49 +0100

    Initial Commit

---
+++ Application/Lazarbeam/Lazarbeam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lazarbeam
+//
+//  Created by Kjell Verschuren on 01/12/2016.
+//  Copyright © 2016 Kjell Verschuren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
